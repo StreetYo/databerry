0aff9d6d9f5c16f190636afacf2f1f040bf9c870
Nariman <nngasanov@gmail.com>
2023-11-06 20:16:51 +0500

    Fix

--- packages/prisma/seed.ts
+++ packages/prisma/seed.ts
@@ -8,6 +8,68 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 async function main() {
   try {
+    const products = await stripe.products.list();
+    const prices = await stripe.prices.list();
+
+    await Promise.all(
+      products.data.map((each) =>
+        prisma.product.upsert({
+          where: {
+            id: each.id,
+          },
+          create: {
+            id: each.id,
+            name: each.name,
+            description: each.description,
+            active: each.active,
+            image: each.images?.[0],
+            metadata: each.metadata,
+          },
+          update: {
+            name: each.name,
+            description: each.description,
+            active: each.active,
+            image: each.images?.[0],
+            metadata: each.metadata,
+          },
+        })
+      )
+    );
+
+    await Promise.all(
+      prices.data.map((each) =>
+        prisma.price.upsert({
+          where: {
+            id: each.id,
+          },
+          create: {
+            type: each.type as any,
+            id: each.id,
+            currency: each.currency,
+            active: each.active,
+            unitAmount: each.unit_amount,
+            interval: each.recurring?.interval,
+            interval_count: each.recurring?.interval_count,
+            trial_period_days: each.recurring?.trial_period_days,
+            product: {
+              connect: {
+                id: each.product as string,
+              },
+            },
+          },
+          update: {
+            type: each.type as any,
+            currency: each.currency,
+            active: each.active,
+            unitAmount: each.unit_amount,
+            interval: each.recurring?.interval,
+            interval_count: each.recurring?.interval_count,
+            trial_period_days: each.recurring?.trial_period_days,
+          },
+        })
+      )
+    );
+
     const userId = 'clnol0gtc000208jlgp3p83av';
     const freeOrgId = 'clnokxi0p000008jlaxe24av9';
     const premiumOrgId = 'clnol6ij8000308jl1cky5hsy';
@@ -71,68 +133,6 @@ async function main() {
       },
       update: {},
     });
-
-    const products = await stripe.products.list();
-    const prices = await stripe.prices.list();
-
-    await Promise.all(
-      products.data.map((each) =>
-        prisma.product.upsert({
-          where: {
-            id: each.id,
-          },
-          create: {
-            id: each.id,
-            name: each.name,
-            description: each.description,
-            active: each.active,
-            image: each.images?.[0],
-            metadata: each.metadata,
-          },
-          update: {
-            name: each.name,
-            description: each.description,
-            active: each.active,
-            image: each.images?.[0],
-            metadata: each.metadata,
-          },
-        })
-      )
-    );
-
-    await Promise.all(
-      prices.data.map((each) =>
-        prisma.price.upsert({
-          where: {
-            id: each.id,
-          },
-          create: {
-            type: each.type as any,
-            id: each.id,
-            currency: each.currency,
-            active: each.active,
-            unitAmount: each.unit_amount,
-            interval: each.recurring?.interval,
-            interval_count: each.recurring?.interval_count,
-            trial_period_days: each.recurring?.trial_period_days,
-            product: {
-              connect: {
-                id: each.product as string,
-              },
-            },
-          },
-          update: {
-            type: each.type as any,
-            currency: each.currency,
-            active: each.active,
-            unitAmount: each.unit_amount,
-            interval: each.recurring?.interval,
-            interval_count: each.recurring?.interval_count,
-            trial_period_days: each.recurring?.trial_period_days,
-          },
-        })
-      )
-    );
   } catch (err) {
     console.log('prisma seed err', err);
     throw err;
