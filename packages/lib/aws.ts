00c97066fe2d4ca798892f3453ad1ee4a21e6cad
Nariman <nngasanov@gmail.com>
2023-11-07 20:39:57 +0500

    Add aws log

--- packages/lib/aws.ts
+++ packages/lib/aws.ts
@@ -1,5 +1,17 @@
 import { S3 } from 'aws-sdk';
 
+console.log(
+  'aws',
+  process.env.APP_AWS_S3_ENDPOINT,
+  process.env.APP_AWS_S3_ENDPOINT ? 't' : 'f',
+  ...(process.env.APP_AWS_S3_ENDPOINT
+    ? {
+        endpoint: process.env.APP_AWS_S3_ENDPOINT,
+        s3ForcePathStyle: process.env.APP_AWS_S3_FORCE_PATH_STYLE === 'true',
+      }
+    : {})
+);
+
 export const s3 = new S3({
   signatureVersion: 'v4',
   accessKeyId: process.env.APP_AWS_ACCESS_KEY,
